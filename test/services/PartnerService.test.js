eae3d3578545693d5e2bbd781de69a6bf153df9f
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-29 17:59:37 -0500

    Add Test for getEmailsWithCertification

--- test/services/PartnerService.test.js
+++ test/services/PartnerService.test.js
@@ -1,7 +1,7 @@
 const PartnerService = require("../../lib/services/PartnerService");
 
 describe("Unit tests for Partner Service", () => {
-    test("1. Use the Partner service", () => {
+    test("1. Use the PartnerService method getAllStudents", () => {
         const partners = [{
             "id": "6264d5d89f1df827eb84bb23",
             "name": "Warren",
@@ -16,4 +16,19 @@ describe("Unit tests for Partner Service", () => {
           }];
           expect(PartnerService.getAllStudents(partners)).toStrictEqual(partners);
     });
+    test("2. Use the PartnerService method getEmailsWithCertification", () => {
+      const partners = [{
+          "id": "6264d5d89f1df827eb84bb23",
+          "name": "Warren",
+          "email": "Todd@visualpartnership.xyz",
+          "credits": 508,
+          "enrollments": [
+            "Visual Thinking Intermedio",
+            "Visual Thinking Avanzado"
+          ],
+          "previousCourses": 1,
+          "haveCertification": true
+        }];
+        expect(PartnerService.getEmailsWithCertification(partners)).toBe(partners[0].email);
+  });
 });
