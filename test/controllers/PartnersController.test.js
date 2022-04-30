8c1d2d6013898a7082758a8999cd405944b6c9f2
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-29 21:29:49 -0500

    Fixing style with linter-fix

--- test/controllers/PartnersController.test.js
+++ test/controllers/PartnersController.test.js
@@ -1,13 +1,13 @@
 const PartnersController = require("./../../lib/controllers/PartnersController");
-const Reader = require("./../../lib/utils/Reader")
+const Reader = require("./../../lib/utils/Reader");
 
 describe("Unit tests for Partners Controller", () => {
     test("1. Use the Partners Controller", () => {
         const partners = Reader.readJsonFile("data/students-db.json");
-          expect(PartnersController.getAllStudents()).toStrictEqual(partners);
-          const emailsWithCertification = PartnersController.getEmailsWithCertification();
-          expect(emailsWithCertification).toBeDefined();
-          expect(PartnersController.getStudentsWithCredits(-1)).toStrictEqual(partners);
-          expect(PartnersController.getStudentsWithCredits(10000)).toHaveLength(0);
+        expect(PartnersController.getAllStudents()).toStrictEqual(partners);
+        const emailsWithCertification = PartnersController.getEmailsWithCertification();
+        expect(emailsWithCertification).toBeDefined();
+        expect(PartnersController.getStudentsWithCredits(-1)).toStrictEqual(partners);
+        expect(PartnersController.getStudentsWithCredits(10000)).toHaveLength(0);
     });
 });
--- test/services/PartnerService.test.js
+++ test/services/PartnerService.test.js
@@ -8,74 +8,74 @@ describe("Unit tests for Partner Service", () => {
             "email": "Todd@visualpartnership.xyz",
             "credits": 508,
             "enrollments": [
-              "Visual Thinking Intermedio",
-              "Visual Thinking Avanzado"
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
             ],
             "previousCourses": 1,
             "haveCertification": true
-          }];
-          expect(PartnerService.getAllStudents(partners)).toStrictEqual(partners);
+        }];
+        expect(PartnerService.getAllStudents(partners)).toStrictEqual(partners);
     });
     test("2. Use the PartnerService method getEmailsWithCertification", () => {
-      const partner1 = {
-        "id": "6264d5d89f1df827eb84bb23",
-        "name": "Warren",
-        "email": "Todd@visualpartnership.xyz",
-        "credits": 508,
-        "enrollments": [
-          "Visual Thinking Intermedio",
-          "Visual Thinking Avanzado"
-        ],
-        "previousCourses": 1,
-        "haveCertification": true
-      };
-      const partner2 = {
-        "id": "6264d5d85cf81c496446b67f",
-        "name": "Lucinda",
-        "email": "Sexton@visualpartnership.xyz",
-        "credits": 677,
-        "enrollments": [
-          "Visual Thinking Avanzado"
-        ],
-        "previousCourses": 6,
-        "haveCertification": false
-      };
-      const partners = [partner1,partner2];
-      const partnerArray1 = [partner1];
+        const partner1 = {
+            "id": "6264d5d89f1df827eb84bb23",
+            "name": "Warren",
+            "email": "Todd@visualpartnership.xyz",
+            "credits": 508,
+            "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 1,
+            "haveCertification": true
+        };
+        const partner2 = {
+            "id": "6264d5d85cf81c496446b67f",
+            "name": "Lucinda",
+            "email": "Sexton@visualpartnership.xyz",
+            "credits": 677,
+            "enrollments": [
+                "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 6,
+            "haveCertification": false
+        };
+        const partners = [partner1,partner2];
+        const partnerArray1 = [partner1];
 
-      expect(PartnerService.getAllCertifications(partners)).toStrictEqual(partnerArray1);
-      expect(PartnerService.getEmails(partners)).toStrictEqual([partner1.email, partner2.email]);
-      expect(PartnerService.getEmailsWithCertification(partners)).toMatchObject([partner1.email]);
-  });
-  test("3. Use the PartnerService method getStudentsWithCredits", () => {
-    const partner1 = {
-      "id": "6264d5d89f1df827eb84bb23",
-      "name": "Warren",
-      "email": "Todd@visualpartnership.xyz",
-      "credits": 508,
-      "enrollments": [
-        "Visual Thinking Intermedio",
-        "Visual Thinking Avanzado"
-      ],
-      "previousCourses": 1,
-      "haveCertification": true
-    };
-    const partner2 = {
-      "id": "6264d5d85cf81c496446b67f",
-      "name": "Lucinda",
-      "email": "Sexton@visualpartnership.xyz",
-      "credits": 677,
-      "enrollments": [
-        "Visual Thinking Avanzado"
-      ],
-      "previousCourses": 6,
-      "haveCertification": false
-    };
-    const partners = [partner1,partner2];
-    const partnerArray2 = [partner2];
+        expect(PartnerService.getAllCertifications(partners)).toStrictEqual(partnerArray1);
+        expect(PartnerService.getEmails(partners)).toStrictEqual([partner1.email, partner2.email]);
+        expect(PartnerService.getEmailsWithCertification(partners)).toMatchObject([partner1.email]);
+    });
+    test("3. Use the PartnerService method getStudentsWithCredits", () => {
+        const partner1 = {
+            "id": "6264d5d89f1df827eb84bb23",
+            "name": "Warren",
+            "email": "Todd@visualpartnership.xyz",
+            "credits": 508,
+            "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 1,
+            "haveCertification": true
+        };
+        const partner2 = {
+            "id": "6264d5d85cf81c496446b67f",
+            "name": "Lucinda",
+            "email": "Sexton@visualpartnership.xyz",
+            "credits": 677,
+            "enrollments": [
+                "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 6,
+            "haveCertification": false
+        };
+        const partners = [partner1,partner2];
+        const partnerArray2 = [partner2];
 
-    expect(PartnerService.getStudentsWithCredits(partners,500)).toStrictEqual(partners);
-    expect(PartnerService.getStudentsWithCredits(partners,600)).toStrictEqual(partnerArray2);
-    expect(PartnerService.getStudentsWithCredits(partners,700)).toHaveLength(0);
-});
+        expect(PartnerService.getStudentsWithCredits(partners,500)).toStrictEqual(partners);
+        expect(PartnerService.getStudentsWithCredits(partners,600)).toStrictEqual(partnerArray2);
+        expect(PartnerService.getStudentsWithCredits(partners,700)).toHaveLength(0);
+    });
 });
