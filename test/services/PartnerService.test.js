c4e57fece1ff7e90f8f1114fed075034b7297a14
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-29 20:14:37 -0500

    Add extra tests for auxiliary methods

--- test/services/PartnerService.test.js
+++ test/services/PartnerService.test.js
@@ -17,18 +17,34 @@ describe("Unit tests for Partner Service", () => {
           expect(PartnerService.getAllStudents(partners)).toStrictEqual(partners);
     });
     test("2. Use the PartnerService method getEmailsWithCertification", () => {
-      const partners = [{
-          "id": "6264d5d89f1df827eb84bb23",
-          "name": "Warren",
-          "email": "Todd@visualpartnership.xyz",
-          "credits": 508,
-          "enrollments": [
-            "Visual Thinking Intermedio",
-            "Visual Thinking Avanzado"
-          ],
-          "previousCourses": 1,
-          "haveCertification": true
-        }];
-        expect(PartnerService.getEmailsWithCertification(partners)[0]).toStrictEqual(partners[0].email);
+      const partner1 = {
+        "id": "6264d5d89f1df827eb84bb23",
+        "name": "Warren",
+        "email": "Todd@visualpartnership.xyz",
+        "credits": 508,
+        "enrollments": [
+          "Visual Thinking Intermedio",
+          "Visual Thinking Avanzado"
+        ],
+        "previousCourses": 1,
+        "haveCertification": true
+      };
+      const partner2 = {
+        "id": "6264d5d85cf81c496446b67f",
+        "name": "Lucinda",
+        "email": "Sexton@visualpartnership.xyz",
+        "credits": 677,
+        "enrollments": [
+          "Visual Thinking Avanzado"
+        ],
+        "previousCourses": 6,
+        "haveCertification": false
+      };
+      const partners = [partner1,partner2];
+      const partnerArray1 = [partner1];
+
+      expect(PartnerService.getAllCertifications(partners)).toStrictEqual(partnerArray1);
+      expect(PartnerService.getEmails(partners)).toStrictEqual([partner1.email, partner2.email])
+      expect(PartnerService.getEmailsWithCertification(partners)[0]).toStrictEqual(partners[0].email);
   });
 });
