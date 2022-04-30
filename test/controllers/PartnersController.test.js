8d7207018fb55f0aadd05dfbf9eac0db570349ab
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-29 20:22:02 -0500

    Create test for getEmailWithCertification in PartnersController

--- test/controllers/PartnersController.test.js
+++ test/controllers/PartnersController.test.js
@@ -5,5 +5,8 @@ describe("Unit tests for Partners Controller", () => {
     test("1. Use the Partners Controller", () => {
         const partners = Reader.readJsonFile("data/students-db.json");
           expect(PartnersController.getAllStudents()).toStrictEqual(partners);
+          const emailsWithCertification = PartnersController.getEmailsWithCertification();
+          const validation = emailsWithCertification.every((partner) => partner.haveCertification == true);
+          expect(validation).toBe(true);
     });
 });
